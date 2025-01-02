90c589e5dcf6275ed3c65c990f568bf6cadeeff9
Syxh0wN <leonardobennr@gmail.com>
2025-01-02 13:09:14 -0300

    feat: css finished

--- src/components/Tasks/Task.tsx
+++ src/components/Tasks/Task.tsx
@@ -1,38 +1,89 @@
 import style from "./Task.module.css";
 import clipboard from "../../assets/task/Clipboard.svg";
+import { List } from "./List/List";
+
+const task = [
+  {
+    id: 1,
+    title: "Task Circular",
+    description: "Organize the files for Monday's meeting",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Task Circular",
+    description: "Review the quarterly financial reports",
+    completed: true,
+  },
+  {
+    id: 3,
+    title: "Task Circular",
+    description: "Plan the schedule for the corporate event",
+    completed: false,
+  },
+  {
+    id: 4,
+    title: "Task Circular",
+    description: "Send follow-up emails to clients",
+    completed: true,
+  },
+];
+
 
 export const Task = () => {
+
+  const totalTasks = task.length;
+  const completedTasks = task.filter((t) => t.completed).length;
+
+  const handleDeleteTask = (tId: number) => {
+    console.log(`Deleting task with ID: ${tId}`);
+    
+  };
+
+  const handleChangeTask = (tId: number) => {
+    console.log(`Changing task with ID: ${tId}`);
+  };
+
   return (
-    <div className={style.taskContainerCenter}>
+    <section className={style.taskContainerCenter}>
       <div className={style.taskContainer}>
         <div className={style.taskHeader}>
           <div>
             <span className={style.created}>Tarefas criadas</span>
             <div>
-              <strong>0</strong>
+              <strong>{totalTasks}</strong>
             </div>
           </div>
 
           <div>
-            <span className={style.completed}>Concluidas</span>
+            <span className={style.completed}>Concluídas</span>
             <div>
-              <strong>0</strong>
+              <strong>{completedTasks}</strong>
             </div>
           </div>
         </div>
-        <div className={style.taskList}>
-          <img src={clipboard} alt="clipboard" />
 
-          <div>
+        {totalTasks === 0 ? (
+          <div className={style.taskList}>
+            <img src={clipboard} alt="clipboard" />
+
             <div>
-              <strong>Você ainda não tem tarefas cadastradas</strong>
-            </div>
-            <div>
-              <span>Crie tarefas e organize seus itens a fazer</span>
+              <div>
+                <strong>Você ainda não tem tarefas cadastradas</strong>
+              </div>
+              <div>
+                <span>Crie tarefas e organize seus itens a fazer</span>
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <ul>
+            {task.map((t) => (
+              <List key={t.id} task={t} onDelete={handleDeleteTask} onChange={handleChangeTask}/>
+            ))}
+          </ul>
+        )}
       </div>
-    </div>
+    </section>
   );
 };
