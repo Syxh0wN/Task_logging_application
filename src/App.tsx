ee04c825d7fd9d533720295f48c726a1dd7c6101
Syxh0wN <leonardobennr@gmail.com>
2025-01-02 11:00:45 -0300

    feat: css finish no task's

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header/Header";
 import { Search } from "./components/Search/Search";
+import { Task } from "./components/Tasks/Task";
 import "./styled/global.css";
 
 export function App() {
@@ -7,6 +8,7 @@ export function App() {
     <div>
       <Header />
       <Search />
+      <Task />
     </div>
   );
 }
--- src/components/Tasks/Task.tsx
+++ src/components/Tasks/Task.tsx
@@ -1,1 +1,38 @@
-*
+import style from "./Task.module.css";
+import clipboard from "../../assets/task/Clipboard.svg";
+
+export const Task = () => {
+  return (
+    <div className={style.taskContainerCenter}>
+      <div className={style.taskContainer}>
+        <div className={style.taskHeader}>
+          <div>
+            <span className={style.created}>Tarefas criadas</span>
+            <div>
+              <strong>0</strong>
+            </div>
+          </div>
+
+          <div>
+            <span className={style.completed}>Concluidas</span>
+            <div>
+              <strong>0</strong>
+            </div>
+          </div>
+        </div>
+        <div className={style.taskList}>
+          <img src={clipboard} alt="clipboard" />
+
+          <div>
+            <div>
+              <strong>Você ainda não tem tarefas cadastradas</strong>
+            </div>
+            <div>
+              <span>Crie tarefas e organize seus itens a fazer</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
