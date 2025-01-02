3e499424c66959f9b035c16df90da7a7b8ea3d4e
Syxh0wN <leonardobennr@gmail.com>
2025-01-02 08:41:11 -0300

    feat: Logo header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import { Header } from "./components/Header/Header";
 import "./styled/global.css";
 
 export function App() {
   return (
-    <h1>Ola</h1>
+    <Header />
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,1 +1,10 @@
-*
+import style from './Header.module.css';
+import logo from '../../assets/logo/Logo.svg';
+
+export const Header = () => {
+  return (
+    <header className={style.header}>
+        <img src={logo} alt="logo" />
+    </header>
+  );
+};
